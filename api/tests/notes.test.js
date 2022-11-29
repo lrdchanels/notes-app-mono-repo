ae68a719ce3138a2a65b9d70ed31edd2c50dafb5
sergicanals <sergicanals@protonmail.com>
2022-11-29 19:59:31 +0100

    enf

--- api/tests/notes.test.js
+++ api/tests/notes.test.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import supertest from 'supertest'
 import { app } from '../index.js'
 
@@ -7,4 +8,14 @@ test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
+})
+
+beforeAll(done => {
+  done()
+})
+
+afterAll(done => {
+  // Closing the DB connection allows Jest to exit successfully.
+  mongoose.connection.close()
+  done()
 })
