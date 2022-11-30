e6551fb44d6455ff9c37e77a55f7f3317863c578
lrdchanels <118404890+lrdchanels@users.noreply.github.com>
2022-11-30 06:57:07 +0100

    global

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ import './mongo.js'
 import express from 'express'
 import cors from 'cors'
 
-import { Note } from './models/Note.js'
+import { usersRouter } from './routes/users.route.js'
+import { notesRouter } from './routes/notes.route.js'
 import { notFound } from './middleware/notFound.js'
 import { handleErrors } from './middleware/handleErrors.js'
 
@@ -17,78 +18,15 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello Wold</h1>')
 })
 
-app.get('/api/notes', (request, response, next) => {
-  Note.find({})
-    .then(notes => {
-      response.json(notes)
-    })
-    .catch(next)
-})
-
-app.post('/api/notes', async (request, response, next) => {
-  const note = request.body
-
-  if (!note || !note.content) {
-    return response.status(400).json({
-      error: 'note is missing'
-    })
-  }
-
-  const noteToInsert = new Note({
-    content: note.content,
-    date: new Date(),
-    important: note.important ?? false
-  })
-
-  noteToInsert.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(next)
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-  const { id } = request.params
-
-  Note.findById(id)
-    .then(note => {
-      response.json(note)
-    })
-    .catch(next)
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  const { id } = request.params
-
-  Note.findByIdAndDelete(id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(next)
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const { id } = request.params
-  const note = request.body
-
-  const newNoteInfo = {
-    content: note.content,
-    important: note.important
-  }
-
-  Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
-    .then(note => {
-      response.status(200).json(note)
-    })
-    .catch(next)
-})
+app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(notFound)
 app.use(handleErrors)
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-export { app }
+export { app, server }
